8ce86245ba7392cbc0fdb561e4c9c67221502629
unknown <alosbjk07@gmail.com>
2022-06-12 03:11:16 +0200

    12.06.2022   03:11

--- teacher.py
+++ teacher.py
@@ -20,15 +20,27 @@ class Teacher(QMainWindow):
         self.pushButton_grades_edit_edit.clicked.connect(self.edit_grade)
         self.pushButton_grades_remove_remove.clicked.connect(self.remove_grade)
         self.show()
+        self.load_data()
     
-    
-    
+    def load_data(self):
+        conn = psycopg2.connect(host= 'localhost',database = 'school_management',user = 'postgres',password = '123')
+        cur = conn.cursor()
+        cur.execute("select first_name,last_name, password, user_name, teacher_email  from teachers where teacher_id = 1")
+        teacher = cur.fetchall()
+        for r in teacher:
+            self.lineEdit_first_name_teacher.insert(r[0])
+            self.lineEdit_lastname_teacher.insert(r[1])
+            self.lineEdit_password_teacher.insert(r[2])
+            self.lineEdit_username_teacher.insert(r[3])
+            self.lineEdit_emai_teacher.insert(r[4])
+
+
     def add_lesson(self):
-        conn = psycopg2.connect(host= 'localhost',database = 'school_management',user = 'postgres',password = '12345')
+        conn = psycopg2.connect(host= 'localhost',database = 'school_management',user = 'postgres',password = '123')
         cur = conn.cursor()
         lesson_name = self.lineEdit_lesson_add_newlesson.text()
-        qry = "insert into lessons(name) values(%s)"
-        value = (lesson_name)
+        qry = "insert into lessons (name) values(%s)"
+        value = (lesson_name,) # (burai tuple olamasi gerekmis sonuna virgul ekleyince calist)
         cur.execute(qry,value)
         conn.commit()
         # TypeError: not all arguments converted during string formatting
@@ -38,10 +50,22 @@ class Teacher(QMainWindow):
         pass
 
     def edit(self):
-        pass
+        conn = psycopg2.connect(host= 'localhost',database = 'school_management',user = 'postgres',password = '123')
+        cur = conn.cursor()
+        current_name = self.lineEdit_Lesson_Edit_1.text()
+        new_name = self.lineEdit_Lesson_Edit_2.text()
+  
+        cur.execute("update lessons set name = %s where name = %s",(new_name,current_name))
+        conn.commit()
+
 
     def remove_lesson(self):
-        pass
+        conn = psycopg2.connect(host= 'localhost',database = 'school_management',user = 'postgres',password = '123')
+        cur = conn.cursor()
+        remove = self.lineEdit_lesson_remove_lesson.text()
+        cur.execute("delete from lessons where name = %s",(remove,))
+
+        conn.commit()
 
     def remove_student(self):
         pass
@@ -58,13 +82,14 @@ class Teacher(QMainWindow):
     def remove_grade(self):
         pass
 
+if (__name__ == '__main__'):
 # Main App
-app=QApplication(sys.argv)
-mainwindow=Teacher()
-widget=QtWidgets.QStackedWidget()
-widget.addWidget(mainwindow)
-# window = Ui_teacher_functions()
-widget.setFixedWidth(800)
-widget.setFixedHeight(800)
-widget.show()
-app.exec_()
+    app=QApplication(sys.argv)
+    mainwindow=Teacher()
+    widget=QtWidgets.QStackedWidget()
+    widget.addWidget(mainwindow)
+    # window = Ui_teacher_functions()
+    widget.setFixedWidth(800)
+    widget.setFixedHeight(800)
+    widget.show()
+    app.exec_()
